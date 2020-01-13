52d8f7a28dd6d4fa2d0653bee648ab7080568566
hsh814 <hsh814@naver.com>
2020-01-13 17:26:22 +0900

    double.hs

--- ch01-function/double.hs
+++ ch01-function/double.hs
@@ -1,1 +1,13 @@
+--Comment is like this
+--input one int and return double of it
 doubleMe x = x + x
+--Function does not need braces: priotity is higher then +-*/
+doubleUs x y = doubleMe x + doubleMe y
+
+--if else: use then, else is necessary
+doubleSmallNum x = if x > 100
+    then x
+    else doubleMe x
+
+--We can use ' in variable and function name.
+doubleSmallNum' x = (if x > 100 then x else doubleMe x) + 1
