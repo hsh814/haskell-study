4653c304043acabaffc4dd44cfd08437279b23fb
hsh814 <hsh814@naver.com>
2020-01-22 20:58:00 +0900

    InductiveType.hs: Binary Search Tree

--- ch07-type_class/InductiveType.hs
+++ ch07-type_class/InductiveType.hs
@@ -33,4 +33,38 @@ infixr 5 +++
 --Binary Search Tree
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving  (Show)
 
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+--Tree with only one node
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right) 
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+--insert node into tree: at leaf node
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+
+numsTree :: Tree Int
+numsTree = foldr treeInsert EmptyTree [8,6,4,1,7,3,5]
+{-
+    Node 5 (Node 3 (Node 1 EmptyTree EmptyTree) (Node 4 EmptyTree EmptyTree)) 
+    (Node 7 (Node 6 EmptyTree EmptyTree) (Node 8 EmptyTree EmptyTree))
+    *Main> treeElem 8 numsTree
+    True
+    *Main> treeElem 12 numsTree
+    False
+-}
+
+
+
+
 
