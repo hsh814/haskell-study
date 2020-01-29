fe3ca8b56edb1e820bb0e504699abb5066a54c52
hsh814 <hsh814@naver.com>
2020-01-29 23:02:31 +0900

    FileStream.hs

--- ch09-more_IO/FileStream.hs
+++ ch09-more_IO/FileStream.hs
@@ -1,7 +1,8 @@
 --Let's read and write file without terminal
 import qualified System.IO as SIO
+import qualified Control.Exception as CE
 
-main = do 
+main0 = do 
     handle <- SIO.openFile "text.txt" SIO.ReadMode
     contents <- SIO.hGetContents handle
     putStr contents
@@ -38,9 +39,24 @@ I'm really thinking I can call this place home
 -}
 
 --withFile :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+main1 = do
+    SIO.withFile "text.txt" SIO.ReadMode (\handle -> do
+        contents <- SIO.hGetContents handle
+        putStr contents)
 
+--bracket type: exception -> close the resource
+--braket :: IO a -> (a -> IO b) -> (a -> IO c) -> IO c
+--first parameter: get resource like file handler
+--second: release resource
+--third: use that resource -> do something
+withFile1 :: FilePath -> SIO.IOMode -> (SIO.Handle -> IO a) -> IO a
+withFile1 name mode f = CE.bracket (SIO.openFile name mode)
+    (\handle -> SIO.hClose handle)
+    (\handle -> f handle)
 
-
-
+--use file as string: readFile, writeFile, appendFile
+main = do
+    contents <- readFile "text.txt"
+    putStr contents
 
 
