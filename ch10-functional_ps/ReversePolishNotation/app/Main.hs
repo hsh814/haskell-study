e7c07875249db0a6371ca2da9531955a5e15b978
hsh814 <hsh814@naver.com>
2020-01-30 23:57:23 +0900

    RPM

--- ch10-functional_ps/ReversePolishNotation/app/Main.hs
+++ ch10-functional_ps/ReversePolishNotation/app/Main.hs
@@ -1,4 +1,30 @@
 --prefix calculation
 
 solveRPN :: String -> Double
-solveRPN
+solveRPN expression = head (foldl foldingFunction [] (words expression))
+    where
+        foldingFunction (x:y:ys) "*" = (y * x) : ys
+        foldingFunction (x:y:ys) "+" = (y + x) : ys
+        foldingFunction (x:y:ys) "-" = (y - x) : ys
+        foldingFunction (x:y:ys) "/" = (y / x) : ys
+        foldingFunction (x:y:ys) "^" = (y ** x) : ys
+        foldingFunction (x:xs) "ln" = log x : xs
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = read numberString:xs
+{-
+    *Main> solveRPN "10 4 3 + 2 * -"
+    -4.0
+    *Main> solveRPN "2.7 ln"
+    0.9932517730102834
+    *Main> solveRPN "10 10 10 10 sum 4 /"
+    10.0
+    *Main> solveRPN "10 2 ^"
+    100.0
+-}
+
+{-
+main = do
+    putStr "Input expression: "
+    expression <- getLine
+    putStrLn "Result: " ++ show $ solveRPN expression
+-}
