95c1ac7008094f80bb47c4d8222756374141c387
hsh814 <hsh814@naver.com>
2020-02-03 22:08:51 +0900

    temp

--- ch11-applicative_functor/Functors/Functors.hs
+++ ch11-applicative_functor/Functors/Functors.hs
@@ -1,2 +1,26 @@
 --Functors can be maaped like list, tree, Maybe...
---
+--Let's look at IO instance
+{-
+instance Functor IO where
+    fmap f action = do
+        result <- action
+        return (f result)
+-}
+
+import qualified Data.Char as DC
+import qualified Data.List as DL
+
+
+main0 = do
+    line <- getLine
+    let line' = reverse line
+    putStrLn $ "Input string: " ++ line
+    putStrLn $ "Backward: " ++ line'
+{-
+    this is my input
+    Input string: this is my input
+    Backward: tupni ym si siht
+-}
+
+main = do line <- fmap (DL.intersperse '-' . reverse . map DC.toUpper) getLine
+            putStrLn line
