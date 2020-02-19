2bb285dc01bd6553847154158a93f62cd09c782d
hsh814 <hsh814@naver.com>
2020-02-19 19:25:25 +0900

    Tree.hs

--- ch15-zipper/README.md
+++ ch15-zipper/README.md
@@ -1,4 +1,30 @@
 # ch15-zipper
 
 ## Contents
+- [Tree](#Tree)
+- [List](#List)
+- [FileSystem](#FileSystem)
+
+## [Tree](./app/Tree.hs)
+
+Due to reference transparency, haskell can't change value.
+When we use tree, if we change value, it's just returning new tree with slightly changed values.
+Then, how can we use tree?
+
+
+
+
+
+
+
+## [List](./app/List.hs)
+
+
+
+
+
+
+
+## [FileSystem](./app/FileSystem.hs)
+
 
---
+++ ch15-zipper/app/Tree.hs
@@ -0,0 +1,74 @@
+import qualified Data.Foldable as F
+import Data.Monoid
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving  (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+--Tree with only one node
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+--insert node into tree: at leaf node
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+    
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    foldMap f (Node x l r) =
+        (F.foldMap f l) `mappend` (f x) `mappend` (F.foldMap f r)
+
+
+testTree :: Tree Int
+testTree =
+    Node 5
+    (
+        Node 3
+            (Node 1 EmptyTree EmptyTree)
+            (Node 6 EmptyTree EmptyTree)
+    )
+    (
+        Node 9
+            (Node 8 EmptyTree EmptyTree)
+            (Node 10 EmptyTree EmptyTree)
+    )
+
+freeTree :: Tree Char
+freeTree =
+    Node 'P'
+    (
+        Node 'O'
+        (
+            Node 'L'
+                singleton 'N'
+                singleton 'T'
+        )
+        (
+            Node 'Y'
+                singleton 'S'
+                singleton 'A'
+        )
+    )
+    (
+        Node 'L'
+        (
+            Node 'W'
+                singleton 'C'
+                singleton 'R'
+        )
+        (
+            Node 'A'
+                singleton 'A'
+                singleton 'C'
+        )
+    )
